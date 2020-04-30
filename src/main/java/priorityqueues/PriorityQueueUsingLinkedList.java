8bf112896d08a2301a3f400ba270069484238cd9
nikhilgoyal104 <nikhilgoyal104ah4@gmail.com>
2020-05-01 03:34:15 +0530

    code cleanup

--- src/main/java/priorityqueues/PriorityQueueUsingLinkedList.java
+++ src/main/java/priorityqueues/PriorityQueueUsingLinkedList.java
@@ -6,14 +6,13 @@ import lombok.Data;
 @Data
 class Node {
   int data;
-  // lower values indicate higher priority
-  int priority;
+  int priority; // lower value indicate higher priority
   Node next;
 }
 
 
-// linked list implementation time complexities => peek = O(1), push = O(n), pop = O(1)
-// binary heap implementation time complexities => peek = O(1), push = O(logn), pop = O(logn)
+// linked list impl time complexities => peek = O(1), push = O(n), pop = O(1)
+// binary heap impl time complexities => peek = O(1), push = O(logn), pop = O(logn)
 @Data
 public class PriorityQueueUsingLinkedList {
   public static void main(String[] args) {
